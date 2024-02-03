bdc61099313bbf5281d37d227f90e849ab5afeed
unknown <usern5949@gmail.com>
2024-02-03 22:23:27 +0530

    Added root router

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,19 @@
 const express = require("express")
+require("dotenv").config()
 
 //express app
 const app = express()
 
+//routes
+app.get("/", (req, res)=>{
+    res.json({msg: "Welcome to the app!"})
+}) 
+
 //listen for requests
-app.listen(4000, ()=>{
-    console.log("Listening on port 4000")
+// app.listen(4000, ()=>{
+//     console.log("Listening on port 4000!!!")
+// })
+
+app.listen(process.env.PORT, ()=>{
+    console.log(`Listening on port ${process.env.PORT}!!!`)
 })
