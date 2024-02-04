9a1376bf7bf983f69002d44b99b9a1c1b257c931
unknown <usern5949@gmail.com>
2024-02-04 15:15:26 +0530

    Enabled default formatting

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,40 +1,40 @@
-const express = require("express")
-const Workout = require("../models/workoutModel")
-const router = express.Router()
+const express = require("express");
+const Workout = require("../models/workoutModel");
+const router = express.Router();
 
 //Workout routers
 //GET all workouts
-router.get("/", (req, res)=>{
-    res.json({msg: "GET all workouts"})
-})
+router.get("/", (req, res) => {
+  res.json({ msg: "GET all workouts" });
+});
 
 //GET one workout
-router.get("/:id", (req, res)=>{
-    res.json({msg: "GET one workout"})
-})
+router.get("/:id", (req, res) => {
+  res.json({ msg: "GET one workout" });
+});
 
 //POST new workout
-router.post("/",async (req, res)=>{
-    const {title, load, reps} = req.body
-    try{
-        const workout = await Workout.create({title, load, reps})
-        res.status(200).json(workout)
-    }
-    catch{(error)=>{
-        res.status(400).json({error: error.message})
-    }}
-    // res.json({msg: "POST a new workout"})
-})
+router.post("/", async (req, res) => {
+  const { title, load, reps } = req.body;
+  try {
+    const workout = await Workout.create({ title, load, reps });
+    res.status(200).json(workout);
+  } catch {
+    (error) => {
+      res.status(400).json({ error: error.message });
+    };
+  }
+  // res.json({msg: "POST a new workout"})
+});
 
 //DELETE a workout
-router.delete("/:id", (req, res)=>{
-    res.json({msg: "DELETE a workout"})
-})
+router.delete("/:id", (req, res) => {
+  res.json({ msg: "DELETE a workout" });
+});
 
 //UPDATE a workout
-router.patch("/:id", (req, res)=>{
-    res.json({msg: "UPDATE a workout"})
-})
+router.patch("/:id", (req, res) => {
+  res.json({ msg: "UPDATE a workout" });
+});
 
-
-module.exports = router
+module.exports = router;
