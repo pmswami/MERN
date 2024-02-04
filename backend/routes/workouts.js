96017e4bc6dcb8a3c4cbd362bb970c10ca2da39c
unknown <usern5949@gmail.com>
2024-02-04 15:42:26 +0530

    Added controllers for get and post request

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,31 +1,39 @@
 const express = require("express");
 const Workout = require("../models/workoutModel");
+const {
+  createWorkout,
+  getWorkout,
+  getWorkouts,
+} = require("../controllers/workoutController");
 const router = express.Router();
 
 //Workout routers
 //GET all workouts
-router.get("/", (req, res) => {
-  res.json({ msg: "GET all workouts" });
-});
+// router.get("/", (req, res) => {
+//   res.json({ msg: "GET all workouts" });
+// });
+router.get("/", getWorkouts);
 
 //GET one workout
-router.get("/:id", (req, res) => {
-  res.json({ msg: "GET one workout" });
-});
+// router.get("/:id", (req, res) => {
+//   res.json({ msg: "GET one workout" });
+// });
+router.get("/:id", getWorkout);
 
 //POST new workout
-router.post("/", async (req, res) => {
-  const { title, load, reps } = req.body;
-  try {
-    const workout = await Workout.create({ title, load, reps });
-    res.status(200).json(workout);
-  } catch {
-    (error) => {
-      res.status(400).json({ error: error.message });
-    };
-  }
-  // res.json({msg: "POST a new workout"})
-});
+// router.post("/", async (req, res) => {
+//   //   const { title, load, reps } = req.body;
+//   //   try {
+//   //     const workout = await Workout.create({ title, load, reps });
+//   //     res.status(200).json(workout);
+//   //   } catch {
+//   //     (error) => {
+//   //       res.status(400).json({ error: error.message });
+//   //     };
+//   //   }
+//   // res.json({msg: "POST a new workout"})
+// });
+router.post("/", createWorkout);
 
 //DELETE a workout
 router.delete("/:id", (req, res) => {
