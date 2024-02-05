75bd57536ae1f1b13bc0e0dd69b6b40912ae37f7
unknown <usern5949@gmail.com>
2024-02-06 02:13:19 +0530

    Added email and password validations

--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 const bcrypt = require("bcrypt");
+const validator = require("validator");
 
 const userSchema = Schema(
   {
@@ -21,6 +22,17 @@ const userSchema = Schema(
 
 //statics signup method
 userSchema.statics.signup = async function (email, password) {
+  //validation
+  if (!email || !password) {
+    throw Error("All feilds must be filled");
+  }
+  if (!validator.isEmail(email)) {
+    throw Error("Email is not valid");
+  }
+  if (!validator.isStrongPassword(password)) {
+    throw Error("Password is not strong enough");
+  }
+
   const exists = await this.findOne({ email });
   if (exists) {
     throw Error("Email already in use");
