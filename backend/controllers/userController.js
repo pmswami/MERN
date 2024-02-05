fd000d5fa55f61fa1a38552abd46b291d7094400
unknown <usern5949@gmail.com>
2024-02-06 02:01:59 +0530

    Added user signup feature in backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -6,8 +6,15 @@ const loginUser = (req, res) => {
 };
 
 //signup user
-const signupUser = (req, res) => {
-  res.json({ msg: "Signup User" });
+const signupUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.signup(email, password);
+    res.status(200).json({ email, user });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+  // res.json({ msg: "Signup User" });
 };
 
 module.exports = {
--- backend/models/UserModel.js
+++ backend/models/UserModel.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const bcrypt = require("bcrypt");
 
 const userSchema = Schema(
   {
@@ -17,5 +18,19 @@ const userSchema = Schema(
 );
 //Mongoose will enforce type check by default to all the data that is being inserted into it.
 //Validation will fail if any of the required field is absent in request.
+
+//statics signup method
+userSchema.statics.signup = async function (email, password) {
+  const exists = await this.findOne({ email });
+  if (exists) {
+    throw Error("Email already in use");
+  }
+
+  // salt in password encryption, it adds a random string to the user password. so that even if passwords got leaked it will be so hard to crack it up. This provides an extra layer of security.
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+  const user = await this.create({ email, password: hash });
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema); // exports the schema as model
