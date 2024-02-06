cd7a2e6f0c35e2c5b76babc2c65b9ba01d75c995
unknown <usern5949@gmail.com>
2024-02-06 23:58:41 +0530

    Added signing tokens

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,9 @@
 const User = require("../models/UserModel");
+const jwt = require("jsonwebtoken");
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: "3d" });
+};
 
 //login user
 const loginUser = (req, res) => {
@@ -10,7 +15,11 @@ const signupUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.signup(email, password);
-    res.status(200).json({ email, user });
+
+    //create jwt token
+    const token = createToken(user._id);
+
+    res.status(200).json({ email, token });
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
