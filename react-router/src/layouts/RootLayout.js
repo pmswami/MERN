39156e797c869484c8e617f72244aa791968eac3
unknown <usern5949@gmail.com>
2024-02-09 05:04:06 +0530

    added routing breadcrumbs using useLocation hook

--- react-router/src/layouts/RootLayout.js
+++ react-router/src/layouts/RootLayout.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink, Outlet } from "react-router-dom";
+import Breadcrumbs from "../components/Breadcrumbs";
 
 export default function RootLayout() {
   return (
@@ -15,6 +16,7 @@ export default function RootLayout() {
           <NavLink to="help">Help</NavLink>
           <NavLink to="careers">Careers</NavLink>
         </nav>
+        <Breadcrumbs />
       </header>
       <main>
         <Outlet />
