1c111196d41b8c169f3b3c511a5594f63b91af22
unknown <usern5949@gmail.com>
2024-02-08 23:22:31 +0530

    added react router basics

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+
 function App() {
-  return <div className="App">Net Ninja!</div>;
+  return (
+    <BrowserRouter>
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          {/* normal link component will not provide details whether it is active currently or not.  */}
+          {/* Whereas navlink provide details like active status of that component */}
+          {/* <Link to="/">Home</Link> */}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Routes>
+          {/* <Route path="/" element={<Home />} /> */}
+          <Route index element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </main>
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ react-router/src/pages/About.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="about">
+      <h2>About Us</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin non
+        sagittis odio. Vivamus maximus vitae lectus vel auctor. Vestibulum lorem
+        turpis, dapibus vel tellus non, varius molestie ligula. Mauris viverra
+        maximus dolor, eget blandit magna condimentum non. Sed quis nisl
+        ultricies, suscipit velit nec, ultrices dolor. Nullam eu nisl in neque
+        porttitor rhoncus. Vestibulum sit amet dui lorem. Integer et libero eu
+        leo tincidunt sollicitudin. Proin vel ipsum nec urna dictum
+        sollicitudin.
+      </p>
+
+      <p>
+        Duis elit erat, tristique a aliquet a, facilisis a turpis. Morbi
+        pharetra leo eget mattis accumsan. Nam ut purus ut urna mollis vulputate
+        sit amet vitae lacus. Sed id lacus hendrerit, molestie magna nec,
+        suscipit sem. Cras hendrerit nisi eget purus dictum, ac sodales odio
+        tempor. Proin commodo porta neque, et mattis lacus hendrerit vel.
+        Praesent feugiat eu sem ac consequat. Donec eu efficitur lacus. Nullam
+        pharetra rutrum dapibus. Curabitur placerat a metus vitae ultricies.
+      </p>
+
+      <p>
+        Etiam lorem diam, eleifend vitae ante et, imperdiet rutrum augue. Sed
+        pretium placerat ultricies. Fusce eros nisl, pretium id dolor at,
+        maximus gravida mauris. Morbi ut est porttitor, pharetra purus in,
+        varius purus. Nam porta bibendum feugiat. Sed mattis in libero non
+        placerat. Etiam ut imperdiet dui. Ut ultrices fermentum ante, sed
+        lacinia metus ullamcorper eget. Integer bibendum nibh ut imperdiet
+        bibendum. Morbi elementum lorem nisl, et aliquam velit vulputate id.
+      </p>
+    </div>
+  );
+}
