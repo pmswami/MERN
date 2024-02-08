413f815304411fe0857f9431c68d81ba2b1ca8e0
unknown <usern5949@gmail.com>
2024-02-09 00:00:15 +0530

    added router provider,createBrowserRouter,Outlet

--- react-router/src/App.js
+++ react-router/src/App.js
@@ -1,29 +1,59 @@
-import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+// import { BrowserRouter, Routes, Route, Link, NavLink } from "react-router-dom";
+// import Home from "./pages/Home";
+// import About from "./pages/About";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <header>
+//         <nav>
+//           <h1>Jobarouter</h1>
+//           {/* normal link component will not provide details whether it is active currently or not.  */}
+//           {/* Whereas navlink provide details like active status of that component */}
+//           {/* <Link to="/">Home</Link> */}
+//           <NavLink to="/">Home</NavLink>
+//           <NavLink to="about">About</NavLink>
+//         </nav>
+//       </header>
+//       <main>
+//         <Routes>
+//           {/* <Route path="/" element={<Home />} /> */}
+//           <Route index element={<Home />} />
+//           <Route path="/about" element={<About />} />
+//         </Routes>
+//       </main>
+//     </BrowserRouter>
+//   );
+// }
+
+// export default App;
+
+import {
+  createBrowserRouter,
+  Routes,
+  Route,
+  Link,
+  NavLink,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import RootLayout from "./layouts/RootLayout";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    // we can create route inside another route
+    <Route path="/" element={<RootLayout/>}>
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route index element={<Home />} />
+      <Route path="/about" element={<About />} />
+    </Route>
+  )
+);
 
 function App() {
-  return (
-    <BrowserRouter>
-      <header>
-        <nav>
-          <h1>Jobarouter</h1>
-          {/* normal link component will not provide details whether it is active currently or not.  */}
-          {/* Whereas navlink provide details like active status of that component */}
-          {/* <Link to="/">Home</Link> */}
-          <NavLink to="/">Home</NavLink>
-          <NavLink to="about">About</NavLink>
-        </nav>
-      </header>
-      <main>
-        <Routes>
-          {/* <Route path="/" element={<Home />} /> */}
-          <Route index element={<Home />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </main>
-    </BrowserRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ react-router/src/layouts/RootLayout.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link, NavLink, Outlet } from "react-router-dom";
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          {/* normal link component will not provide details whether it is active currently or not.  */}
+          {/* Whereas navlink provide details like active status of that component */}
+          {/* <Link to="/">Home</Link> */}
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+        </nav>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  );
+}
