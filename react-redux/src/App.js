1ab84e23b85bda853ac179137f4e5796487d7678
unknown <usern5949@gmail.com>
2024-02-10 14:42:21 +0530

    added component types

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -19,23 +19,22 @@
 
 // export default App;
 
-
-import React, { Component } from 'react';
-import Ninjas from './Ninjas'
+import React, { Component } from "react";
+import Ninjas from "./Ninjas";
 
 class App extends Component {
   state = {
     ninjas: [
-      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
-      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
-      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
-    ]
-  }
+      { name: "Ryu", age: 30, belt: "black", id: 1 },
+      { name: "Yoshi", age: 20, belt: "green", id: 2 },
+      { name: "Crystal", age: 25, belt: "pink", id: 3 },
+    ],
+  };
   render() {
     return (
       <div className="App">
         <h1>My first React app</h1>
-        <Ninjas ninjas={this.state.ninjas}/>
+        <Ninjas ninjas={this.state.ninjas} />
       </div>
     );
   }
--- react-redux/src/Ninjas.js
+++ react-redux/src/Ninjas.js
@@ -1,26 +1,44 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-class Ninjas extends Component {
-  render() {
-    const { ninjas } = this.props;
-    const ninjaList = ninjas.map((ninja) => {
-      return (
-        <div className="ninja" key={ninja.id}>
-          <div>Name: {ninja.name}</div>
-          <div>AGe: {ninja.age}</div>
-          <div>Belt: {ninja.belt}</div>
-        </div>
-      );
-    });
+// class Ninjas extends Component {
+//   render() {
+//     const { ninjas } = this.props;
+//     const ninjaList = ninjas.map((ninja) => {
+//       return (
+//         <div className="ninja" key={ninja.id}>
+//           <div>Name: {ninja.name}</div>
+//           <div>AGe: {ninja.age}</div>
+//           <div>Belt: {ninja.belt}</div>
+//         </div>
+//       );
+//     });
+//     return (
+//       <div className="ninja">
+//         {/* <div>Name: {this.props.name}</div>
+//         <div>AGe: {this.props.age}</div>
+//         <div>Belt: {this.props.belt}</div> */}
+//         {ninjaList}
+//       </div>
+//     );
+//   }
+// }
+
+// export default Ninjas;
+
+import React from "react";
+
+function Ninjas({ ninjas }) {
+  //   const { ninjas } = props;
+  const ninjaList = ninjas.map((ninja) => {
     return (
-      <div className="ninja">
-        {/* <div>Name: {this.props.name}</div>
-        <div>AGe: {this.props.age}</div>
-        <div>Belt: {this.props.belt}</div> */}
-        {ninjaList}
+      <div className="ninja" key={ninja.id}>
+        <div>Name: {ninja.name}</div>
+        <div>Age: {ninja.age}</div>
+        <div>Belt: {ninja.belt}</div>
       </div>
     );
-  }
+  });
+  return <div className="ninja">{ninjaList}</div>;
 }
 
 export default Ninjas;
