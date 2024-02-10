c7f50679a9f0380315934c4954293c6394157a44
unknown <usern5949@gmail.com>
2024-02-10 13:15:21 +0530

    added outputting list

--- react-redux/src/App.js
+++ react-redux/src/App.js
@@ -1,9 +1,44 @@
-function App() {
-  return (
-    <div className="App">
-      <h1>My first react app</h1>
-    </div>
-  );
+// import Ninjas from "./Ninjas";
+
+// function App() {
+//   state = {
+//     ninjas: [
+//       { name: "Ryu", age: 30, belt: "black", id: 1 },
+//       { name: "Yoshi", age: 20, belt: "green", id: 2 },
+//       { name: "Crystal", age: 25, belt: "pink", id: 3 },
+//     ],
+//   };
+//   return (
+//     <div className="App">
+//       <h1>My first react app</h1>
+//       {/* <Ninjas name="YOSHI" age="38" belt="balck" /> */}
+//       <Ninjas ninjas={this.state.ninjas} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+import React, { Component } from 'react';
+import Ninjas from './Ninjas'
+
+class App extends Component {
+  state = {
+    ninjas: [
+      { name: 'Ryu', age: 30, belt: 'black', id: 1 },
+      { name: 'Yoshi', age: 20, belt: 'green', id: 2 },
+      { name: 'Crystal', age: 25, belt: 'pink', id: 3 }
+    ]
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>My first React app</h1>
+        <Ninjas ninjas={this.state.ninjas}/>
+      </div>
+    );
+  }
 }
 
 export default App;
