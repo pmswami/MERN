1479c6575f3b0009320f3c78c2d752aec8522a87
unknown <usern5949@gmail.com>
2024-02-10 19:58:39 +0530

    Added basic routes

---
+++ poketimes/src/components/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="container">
+      <h4 className="center">Home</h4>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Repellat
+        quibusdam consectetur accusamus officia, omnis ipsa dolorem molestiae
+        minus ipsum tenetur esse! Excepturi soluta incidunt deserunt dolorum
+        eaque, sapiente fuga alias.
+      </p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ poketimes/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <nav className="nav-wrapper red darken-3">
+      <div className="container">
+        <a className="brand-logo">Poke'Times</a>
+        <ul className="right">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/about">About</a>
+          </li>
+          <li>
+            <a href="/contact">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
