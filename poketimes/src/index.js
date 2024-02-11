efa88ad7f2232455aa5b9e58cc4dc4ea6d970b88
unknown <usern5949@gmail.com>
2024-02-12 00:27:19 +0530

    Added redux createStore

--- poketimes/src/index.js
+++ poketimes/src/index.js
@@ -20,5 +20,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { legacy_createStore as createStore } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers/rootReducer";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
